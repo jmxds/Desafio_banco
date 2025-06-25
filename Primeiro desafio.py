7de7b710f33dfb7ea9f27f4843d360f1ffd1aa90
Tape Face <joao.marco.ximenes@gmail.com>
2025-06-25 20:56:31 -0300

    Update Primeiro desafio.py
    Foram adicionadas as funções de depósito, extrato e algumas melhorias no saque e no funcionamento do sistema

--- Primeiro desafio.py
+++ Primeiro desafio.py
@@ -1,5 +1,7 @@
+import time
+
 Mensagem = f'''
-########################  Bem-Vindo ao Banco Tape Co. #################################
+########################  Bem-Vindo ao Banco Tape Bank #################################
 
                         Menu:
                 
@@ -13,9 +15,26 @@ Mensagem = f'''
 ########################################################################################
 '''
 
-saldo = 10.00
+
+fim = f'''
+########################################################################################
+
+
+
+
+                                Fim da Sessão
+
+
+
+
+#########################################################################################
+
+
+'''
+
+saldo = 0.00
 limite = 500.00
-extrato = ""
+extrato = []
 numero_de_saques = 0
 LIMITE_SAQUES = 3
 
@@ -28,57 +47,80 @@ while True:
     menu = int(input("\n"))
 
     if menu == 1:
-        print("Saque Selecionado")
-        saque = float(input("Insira o Valor que deseja sacar\n"))
-        saque_valido = False
-        while saque_valido == False:
-            if saque > saldo:
-                print(f"Não foi possível realizar o saque pois o valor solicitado é maior do que o valor do saldo.\n Saldo atual: R${saldo:.2f}")
-                choice_1 = input('Deseja sacar outro valor? [s] / [n]\n')
-                
-            
-            if saque > limite:
-                print(f"Não foi possível realizar o saque pois o valor solicitado está acima do valor limite. Valor limite atual: R${limite:.2f}")
-                choice_1 = input('Ainda deseja sacar? [s] / [n]')
-                
-            if saque <= 0.00:
-                print(f"Não é possível sacar o valor {saque:.2f}\n Favor inserir um valor válido")
-                choice_1 = input('Ainda deseja sacar? [s] / [n]')
-                
-            if numero_de_saques>= LIMITE_SAQUES:      
+        if numero_de_saques>= LIMITE_SAQUES:      
                 print(f"Não foi possível realizar o saque pois o limite de saques diários foi atingido.")
-                break
+                time.sleep(3)
+                
+        elif saldo == 0.00:
+            print("Não há saldo para saque")
+            time.sleep(2)
             
-            if saque < limite and saque > 0.00 and saque <= saldo and numero_de_saques <= 3:
-                saque_valido = True  
-            else:    
-                while choice_1 != 's' and choice_1 != 'S' and choice_1 != 'n' and choice_1 != 'N':
-                    print('Insira um valor válido!')
+        else:
+            print("Saque Selecionado")
+            saque = float(input("Insira o Valor que deseja sacar\n"))
+            saque_valido = False
+            
+            while saque_valido == False:
+                if saque > saldo:
+                    print(f"Não foi possível realizar o saque pois o valor solicitado é maior do que o valor do saldo.\n Saldo atual: R$ {saldo:.2f}")
                     choice_1 = input('Deseja sacar outro valor? [s] / [n]\n')
-                if choice_1 == 'n' or choice_1 == 'N':
-                    break
-                elif choice_1 == 's' or choice_1 == 'S':
-                    saque = float(input("Insira o Valor que deseja sacar\n"))
+                    
+                
+                if saque > limite:
+                    print(f"Não foi possível realizar o saque pois o valor solicitado está acima do valor limite. Valor limite atual: R$ {limite:.2f}")
+                    choice_1 = input('Ainda deseja sacar? [s] / [n]\n')
+                    
+                if saque <= 0.00:
+                    print(f"Não é possível sacar o valor R$ {saque:.2f}\n Favor inserir um valor válido")
+                    choice_1 = input('Ainda deseja sacar? [s] / [n]\n')
+                    
             
-        
-        if saque_valido == True:
-            saldo -= saque
-            numero_de_saques+=1
-            print(f" Saque realizado com sucesso \n \n Saldo atual {saldo:.2f}")
+                
+                if saque <= limite and saque > 0.00 and saque <= saldo:
+                    saque_valido = True  
+                else:    
+                    while choice_1 != 's' and choice_1 != 'S' and choice_1 != 'n' and choice_1 != 'N':
+                        print('Insira um valor válido!')
+                        choice_1 = input('Deseja sacar outro valor? [s] / [n]\n')
+                    if choice_1 == 'n' or choice_1 == 'N':
+                        break
+                    elif choice_1 == 's' or choice_1 == 'S':
+                        saque = float(input("Insira o Valor que deseja sacar\n"))
+                
+            
+            if saque_valido == True:
+                saldo -= saque
+                numero_de_saques+=1
+                extrato.append(f"Saque: R$ {saque:.2f} Saldo - R$ {saldo:.2f}")
+                print(f" Saque realizado com sucesso \n \n Saldo atual R$ {saldo:.2f}")
+                time.sleep(3)
 
     elif menu == 2:
         print("Depósito Selecionado")
-        deposito = float(input("Insira o Valor que deseja depositar"))
-        while deposito<=0:
+        deposito = float(input("Insira o Valor que deseja depositar\n"))
+        while deposito<=0.00:
                 print("Insira um valor de depósito válido")
-                deposito = float(input("Insira o Valor que deseja depositar"))
+                deposito = float(input("Insira o Valor que deseja depositar\n"))
         saldo += deposito
+        extrato.append(f"Depósito: R$ {deposito:.2f} Saldo - R$ {saldo:.2f}")
 
-        print(f" Depósito realizado com sucesso \n \n Saldo atual {saldo:.2f}")
+        print(f" Depósito realizado com sucesso \n \n Saldo atual R$ {saldo:.2f}")
+        time.sleep(3)
     elif menu == 3:
-        print("Extrato")
+        if extrato == []:
+            print('Ainda não houve movimentação na conta!')
+            time.sleep(3)
+        else:
+            print(f"\nExtrato:")
+            for trasancoes in extrato:
+                print(trasancoes)
+
+            print(f"\n Saldo Atual: R$ {saldo:.2f}")
+            time.sleep(5)
+            
     elif menu == 4:
-        print("Fim da Seção".center(10,'#'))
+        print(fim)
         break
     else:
         print("Comando Inválido")
+        time.sleep(2)
